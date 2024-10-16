bd27b89d4c469722ce595363512a400f5dc48f36
ZuxiGIT <shim.01@mail.ru>
2024-10-17 01:53:50 +0300

    Added new alias
    Added alias for clean neovim (without plugins)

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,4 +1,5 @@
 alias updg='sudo apt update && sudo apt upgrade -y && sudo apt install --autoremove'
 alias v='nvim'                              # default neovim
 alias lv='NVIM_APPNAME=nvim-latex nvim'     # neovim for latex
+alias cv='NVIM_APPNAME=__ nvim'             # clean neovim (without plugins)
 alias lnvim='NVIM_APPNAME=nvim-latex nvim'  # neovim for latex
