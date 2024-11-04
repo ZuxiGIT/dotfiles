1fbcbcbbc36a1365e382e693d65a52800351d001
ZuxiGIT <shim.01@mail.ru>
2024-11-05 00:36:38 +0300

    Adjusted nvim-latex config
    Added aux_dir option for latexmk vimtex compiler

--- .config/nvim-latex/lua/config/plugins/latex.lua
+++ .config/nvim-latex/lua/config/plugins/latex.lua
@@ -5,6 +5,11 @@ return {
         vim.g.vimtex_view_method = 'zathura'
         vim.g.vimtex_compiler_method = 'latexmk'
 
+        vim.g.vimtex_compiler_latexmk = {
+            ['aux_dir'] = './.aux_dir',
+            ['out_dir'] = './.out_dir',
+        }
+
         vim.o.foldmethod = 'expr'
         vim.o.foldexpr = 'vimtex#fold#level(v:lnum)'
         vim.o.foldtext = 'vimtex#fold#text()'
