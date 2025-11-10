4be634f4e670d289c8aa4985d0be503c2f225fe7
ZuxiGIT <shim.01@mail.ru>
2025-11-10 22:15:02 +0000

    Updated install.sh script

--- install.sh
+++ install.sh
@@ -29,6 +29,43 @@ for file in shell/*; do
     fi
 done
 
-if ask "Install config files?"; then
-    stow -v -t "$HOME" .
+if ask 'Install config files?'; then
+    if [ -z $(command -v stow &> /dev/null) ]; then
+        echo 'Stow is not installed'
+	if ask 'Install stow?'; then
+	    sudo apt install stow
+	fi
+    fi
+    mkdir -p "$HOME/.config"
+    stow  -t "$HOME" "."
+fi
+
+# Install neovim v0.11.5
+if ask 'Install neovim?'; then
+    sudo apt install cmake make build-essential
+    TMP_DIR=$(mktemp -d)
+    git clone -b 'v0.11.5' https://github.com/neovim/neovim $TMP_DIR
+    pushd $TMP_DIR
+    make CMAKE_BUILD_TYPE=Release -j
+    sudo make install -j
+    popd
+fi
+
+# Install tmux
+if ask 'Install tmux?'; then
+    sudo apt install tmux
+    # Install tmux plugin manager
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    echo 'Do not forget to install tmux plugins!'
+fi
+
+# Install tig
+if ask 'Install tig?'; then
+    TMP_DIR=$(mktemp -d)
+    # Install tmux plugin manager
+    git clone https://github.com/jonas/tig $TMP_DIR
+    pushd $TMP_DIR
+    make prefix=/usr/local -j
+    sudo make install prefix=/usr/local -j
+    popd
 fi
